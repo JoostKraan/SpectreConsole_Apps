bb12d0d7979eafc5a1e9b94ccc503a45a67cde46
Joost <2006joost@gmail.com>
2023-12-08 15:21:53 +0100

    nice

---
+++ SpectreRPG/SpectreRPG/Program.cs
@@ -0,0 +1,32 @@
+﻿using Spectre.Console;
+using System.Net.Http.Headers;
+using Spectre.Console.Advanced;
+using Spectre.Console.Extensions;
+using Spectre.Console.Rendering;
+
+namespace SpectreRPG
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ChooseRole();
+        }
+        public static  void ChooseRole()
+        {
+            var rule = new Rule("[yellow]Hello[/]"); 
+
+            var roles = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What is your class going to be?")
+                    .PageSize(3)
+                    .MoreChoicesText("[Orange] (Move up and down")
+                    .AddChoices(new[] {
+                        "Warrior", "Mage", "Archer"
+                    }));
+            AnsiConsole.Write(new Markup($"[seagreen3]You chose [/][bold red]{roles}[/]"));
+            Console.ReadLine();
+        }
+    }
+}
