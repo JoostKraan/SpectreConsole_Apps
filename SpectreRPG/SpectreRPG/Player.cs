97b5d347a7db868fa28e3027073226de0887c91b
Joost <113507203+JoostKraan@users.noreply.github.com>
2023-12-09 14:10:14 +0100

    Added player objects

---
+++ SpectreRPG/SpectreRPG/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+using System.Net.Http.Headers;
+using Spectre.Console.Advanced;
+using Spectre.Console.Extensions;
+using Spectre.Console.Rendering;
+
+namespace SpectreRPG
+{
+    public class Player
+    {
+        public string _name;
+        private int _health;
+        private int _atk;
+        private string _role;
+        private int _Experience;
+
+
+        public Player(string name,int health, int damage, string role, int Experience)
+        {
+            _name = name;
+            _health = health;
+            _atk = damage;
+            _role = role;
+            _Experience = Experience;
+        }
+
+        public void ShowStats()
+        {
+            AnsiConsole.WriteLine();
+            AnsiConsole.WriteLine($"As a {_role} these are your stats...");
+            
+            AnsiConsole.WriteLine($"Your stats : {_name}");
+            AnsiConsole.WriteLine($"Your Damage : {_atk}");
+            AnsiConsole.WriteLine($"Your HP : {_health}");
+            AnsiConsole.WriteLine($"Your XP : {_Experience}");
+        }
+
+
+
+
+
+    }
+}
--- SpectreRPG/SpectreRPG/Program.cs
+++ SpectreRPG/SpectreRPG/Program.cs
@@ -1,31 +1,49 @@
-﻿using Spectre.Console;
+﻿using System.Linq.Expressions;
+using Spectre.Console;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Spectre.Console.Advanced;
 using Spectre.Console.Extensions;
 using Spectre.Console.Rendering;
+using System.Xml.Linq;
 
 namespace SpectreRPG
 
 {
     public class Program
     {
+        public static Panel _panel = new Panel("                       RPG");
+        public static Player _player;
         static void Main(string[] args)
         {
-            ChooseRole();
-        }
-        public static  void ChooseRole()
-        {
-            var rule = new Rule("[yellow]Hello[/]"); 
+            _panel.Border = BoxBorder.Square;
+            _panel.Expand = true;
+            AnsiConsole.Render(_panel);
+            
 
+            PlayerInfo();
+        }
+        public static void PlayerInfo()
+        {
+            string name = AnsiConsole.Prompt(new TextPrompt<string>("[blue]What is your name[/]").PromptStyle("italic seagreen3"));
             var roles = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("What is your class going to be?")
                     .PageSize(3)
-                    .MoreChoicesText("[Orange] (Move up and down")
                     .AddChoices(new[] {
                         "Warrior", "Mage", "Archer"
                     }));
             AnsiConsole.Write(new Markup($"[seagreen3]You chose [/][bold red]{roles}[/]"));
+            if (roles == "Warrior")
+            {
+                _player = new Player(name, 20, 15, "Warrior",0);
+            }
+            _player.ShowStats();
+            
+            
+            
+            AnsiConsole.WriteLine();
             Console.ReadLine();
         }
     }
